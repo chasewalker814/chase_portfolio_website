48605800bd91125c48c71246d37aad8be35d3e5c
chasewalker814 <chasewalker814@gmail.com>
2017-06-07 22:53:01 -0400

    More responsive work
    Updated javascript function to be more functional when media query
    kicks in.

--- JS/style.js
+++ JS/style.js
@@ -9,9 +9,16 @@ $(function() {
       var z = parseInt($(this).css("z-index"), 10);
       max = Math.max(max, z);
     });
+    var width = $(window).width();
+    if (width <= 480) {
+      $(this).children(".box-image").toggleClass("image-hide");
+      $(this).children().toggleClass("showP-small");
+    }
+    else {
+      $(this).toggleClass("grow-div");
+      $(this).children().toggleClass("showP");
+    }
     clickedBox.css("z-index", max + 1);
-    $(this).toggleClass("grow-div");
-    $(this).children().toggleClass("showP");
     console.log(parseInt($(this).css("z-index")));
   });
 });
