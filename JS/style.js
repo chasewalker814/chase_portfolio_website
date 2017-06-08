4e1c64e0757127f3949f44435cf7e90335090dcc
chasewalker814 <chasewalker814@gmail.com>
2017-06-08 16:33:16 -0400

    Adding scroll slide transitions
    divs slide in from the sides when scrolling

--- JS/style.js
+++ JS/style.js
@@ -23,4 +23,65 @@ $(function() {
   });
 });
 
+(function($) {
+  $.fn.visible = function(partial) {
+    var $t = $(this),
+    $w = $(window),
+    viewTop = $w.scrollTop(),
+    viewBottom = viewTop + $w.height(),
+    _top = $t.offset().top,
+    _bottom = _top + $t.height(),
+    compareTop = partial === true ? _bottom : _top,
+    compareBottom = partial === true ? _top : _bottom;
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  };
+})(jQuery);
 
+
+var mods = $(".fly-box").addClass(function () {
+    return $(this).visible(true) && "already-visible";
+});
+$(window).on("scroll", function () {
+  mods.each(function () {
+    $(this).toggleClass("slide-in", $(this).visible(true));
+  });
+});
+
+// var win = $(window);
+// var allMods = $(".module");
+// allMods.each(function(i, el) {
+//   var el = $(el);
+//   if (el.visible(true)) {
+//     el.addClass("already-visible"); 
+//   } 
+// });
+// win.scroll(function(event) {
+//   allMods.each(function(i, el) {
+//     var el = $(el);
+//     if (el.visible(true)) {
+//       el.addClass("slide-in"); 
+//     } 
+//   });
+// });
+
+// var win = $(window);
+
+// var allMods = $(".fly-box");
+
+// allMods.each(function(i, el) {
+//   var el = $(el);
+//   if (el.visible(true)) {
+//     el.addClass("already-visible"); 
+//   } 
+// });
+
+// win.scroll(function(event) {
+  
+//   allMods.each(function(i, el) {
+//     var el = $(el);
+//     if (el.visible(true)) {
+//       el.addClass("slide-in"); 
+//     } 
+//   });
+  
+// });
